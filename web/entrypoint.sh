3db08a81979eb6a3b1c8a2c0460b32a962cde667
guilherme franco <guilherme@guilherme.pti.intra>
2026-04-15 13:41:32 -0300

    deploy

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+set -e
 
-apk add --no-cache git
+apk add --no-cache git ca-certificates
 
 echo "Clonando repositório do GitHub..."
+
 rm -rf /usr/share/nginx/html/*
+rm -rf /tmp/repo
 
 git clone https://github.com/guilhermefranco0013/DevOpsWebServer.git /tmp/repo
 
@@ -11,4 +14,5 @@ echo "Copiando conteúdo da pasta /site..."
 cp -r /tmp/repo/site/* /usr/share/nginx/html/
 
 echo "Iniciando NGINX..."
-nginx -g 'daemon off;'
+
+exec nginx -g 'daemon off;'
