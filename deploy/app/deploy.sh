f3643af6843d210bee3c34e5b1974f5336163809
Guilherme Franco <guilherme.franco.0013@gmail.com>
2026-05-07 14:25:51 -0300

    refactor: update on deploy.sh and deocker-compose.yml

--- deploy/app/deploy.sh
+++ deploy/app/deploy.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 IMAGE_TAG=${1:-web-prod}
+ROLLBACK=${2:-false}
 
 ROOT_DIR="/home/guilherme/devops-lab"
 APP_DIR="$ROOT_DIR/apps"
@@ -20,8 +21,6 @@ else
   PREVIOUS=""
 fi
 
-echo "$IMAGE_TAG" > "$PREVIOUS_FILE"
-
 export IMAGE_TAG
 
 docker compose -p app pull
@@ -32,7 +31,7 @@ docker compose \
 
 echo "⏳ aguardando healthcheck..."
 
-for i in {1..10}; do
+for i in {1..30}; do
   if curl -fsS http://localhost > /dev/null; then
     echo "✅ Aplicação saudável"
     break
@@ -42,14 +41,22 @@ for i in {1..10}; do
 done
 
 if ! curl -fsS http://localhost > /dev/null; then
+
+  if [ "$ROLLBACK" = "true" ]; then
+    echo "❌ Rollback também falhou"
+    exit 1
+  fi
+
   echo "❌ Falha detectada, iniciando rollback..."
 
   if [ -n "$PREVIOUS" ]; then
-    "$DEPLOY_DIR/deploy.sh" "$PREVIOUS"
+    "$DEPLOY_DIR/deploy.sh" "$PREVIOUS" true
   fi
 
   exit 1
 fi
+
+echo "$IMAGE_TAG" > "$PREVIOUS_FILE"
 
 docker image prune -f
 
